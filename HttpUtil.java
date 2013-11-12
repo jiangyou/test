832dc1e2c3f0919504994a6dc8f146ac1c0f88e6
zhuwei <zhuwei@ifeng.com>
2013-11-12 23:07:00 +0800

    add sendPost method

--- HttpUtil.java
+++ HttpUtil.java
@@ -3,4 +3,9 @@ public class HttpUtil{
 	public static String sendGet(String url,Map<String,String>params){
 		return "";
 	}
+
+
+	public static String sendPost(String url,Map<String,String> params){
+	}
+
 }
